ff41fe62e96def75db67be6699083d661c3173f5
Unknown <s3530160@student.rmit.edu.au>
2018-05-28 18:08:40 +1000

    complete junit tests for verifyPassword()

--- src/main/java/com/xphir/rmit/HRSystem/JUnitTests.java
+++ src/main/java/com/xphir/rmit/HRSystem/JUnitTests.java
@@ -6,14 +6,28 @@ import static org.junit.Assert.assertEquals;
 
 public class JUnitTests {
     //create testing data
-    HRAgent testAgent = new HRAgent(001, "John", "Snow", "Administrator", "Science", "John.Snow@rmit.edu.au", "ForTheWatch");
-    CasualStaff testStaff = new CasualStaff(001, "John", "Snow", "Mr", "John.Snow@rmit.edu.au", "COSC1076");
-    Tasks testTask = new Tasks(001, "Take the Iron Throne", "Werteros", "Monday", "War", "10/03/2018", "20/05/2018", "Notes Filler", 55.00, 1130, "COSC1076", "John Snow");
+    HRAgent testAgent;
+    CasualStaff testStaff;
+    Tasks testTask;
 
+    @Before
+    public void setUpAgent(){
+        testAgent = new HRAgent(001, "John", "Snow", "Administrator", "Science", "John.Snow@rmit.edu.au", "ForTheWatch");
+    }
+
+    @Before
+    public void setUpStaff(){
+        testStaff = new CasualStaff(001, "John", "Snow", "Mr", "John.Snow@rmit.edu.au", "COSC1076");
+    }
+
+    @Before
+    public void setUpTask(){
+        testTask = new Tasks(001, "Take the Iron Throne", "Werteros", "Monday", "War", "10/03/2018", "20/05/2018", "Notes Filler", 55.00, 1130, "COSC1076", "John Snow");
+    }
 
     @Test
     //Testing the expected password (this should return true)
-    public void TestPasswordNormalCheck() {
+    public void TestPasswordExactMatch() {
         String testPassword = "ForTheWatch";
         boolean expected = true;
         boolean PasswordCorrect = testAgent.verifyPassword(testPassword);
@@ -31,6 +45,34 @@ public class JUnitTests {
         assertEquals(expected, PasswordCorrect);
     }
 
+    @Test
+    //Testing the expected password (this should return true)
+    public void TestPasswordCompletelyWrong() {
+        String testPassword = "ThisIsWrong";
+        boolean expected = false;
+        boolean PasswordCorrect = testAgent.verifyPassword(testPassword);
+        assertEquals(expected, PasswordCorrect);
+    }
+
+    @Test
+    //Testing a Null password (this should return false)
+    public void TestPasswordNull() {
+        String testPassword = "ThisIsWrong";
+        boolean expected = false;
+        boolean PasswordCorrect = testAgent.verifyPassword(null);
+        assertEquals(expected, PasswordCorrect);
+    }
+
+
+
+
+
+
+
+
+
+
+    
     @Test
     //Testing a simple getter function (NOTE THIS WONT COUNT AS FOR THE ASSIGNMENT)
     public void testGetTaskTitle() {
