91f3c80835a96d5225aa1c4aa015189a1a513927
Unknown <s3530160@student.rmit.edu.au>
2018-05-28 17:55:48 +1000

    adding test "tests"

--- src/main/java/com/xphir/rmit/HRSystem/JUnitTests.java
+++ src/main/java/com/xphir/rmit/HRSystem/JUnitTests.java
@@ -1,16 +1,20 @@
 package com.xphir.rmit.HRSystem;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class JUnitTests {
+    //create testing data
+    HRAgent testAgent = new HRAgent(001, "John", "Snow", "Administrator", "Science", "John.Snow@rmit.edu.au", "ForTheWatch");
+    CasualStaff testStaff = new CasualStaff(001, "John", "Snow", "Mr", "John.Snow@rmit.edu.au", "COSC1076");
+    Tasks testTask = new Tasks(001, "Take the Iron Throne", "Werteros", "Monday", "War", "10/03/2018", "20/05/2018", "Notes Filler", 55.00, 1130, "COSC1076", "John Snow");
+
 
     @Test
     //Testing the expected password (this should return true)
     public void TestPasswordNormalCheck() {
         String testPassword = "ForTheWatch";
-        HRAgent testAgent = new HRAgent(001, "John", "Snow", "Administrator", "Science", "John.Snow@rmit.edu.au", "ForTheWatch");
         boolean expected = true;
         boolean PasswordCorrect = testAgent.verifyPassword(testPassword);
 
@@ -21,10 +25,27 @@ public class JUnitTests {
     //Testing a lowercase password (this should return false)
     public void TestPasswordCaseCheck() {
         String testPassword = "forthewatch";
-        HRAgent testAgent = new HRAgent(001, "John", "Snow", "Administrator", "Science", "John.Snow@rmit.edu.au", "ForTheWatch");
         boolean expected = false;
         boolean PasswordCorrect = testAgent.verifyPassword(testPassword);
 
         assertEquals(expected, PasswordCorrect);
+    }
+
+    @Test
+    //Testing a simple getter function (NOTE THIS WONT COUNT AS FOR THE ASSIGNMENT)
+    public void testGetTaskTitle() {
+        String expected = "Take the Iron Throne";
+        String response = testTask.getTaskTitle();
+
+        assertEquals(expected, response);
+    }
+
+    @Test
+    //Testing a simple getter function (NOTE THIS WONT COUNT AS FOR THE ASSIGNMENT)
+    public void testSetCasualStaffLastName() {
+        String expected = "Targaryen";
+        testStaff.setLastName("Targaryen");
+        String response = testStaff.getLastName();
+        assertEquals(expected, response);
     }
 }
